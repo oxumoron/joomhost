cf12a67baaba69c1d06a49d5e6143b14ddf5a22c
a.suprun <oxymoron.dr@gmail.com>
2019-11-28 14:51:04 +0200

    reviews

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
+$(function() {
+
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true
+    });
 
 });
