faf4816b0ba92981b99039fde016d4bd3d7487ed
a.suprun <oxymoron.dr@gmail.com>
2019-12-04 13:01:58 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ $(function() {
         slidesToShow: 2,
         slidesToScroll: 2,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [{
+            breakpoint: 1106,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }, ]
+    });
+
+    $('.btn__menu').on('click', function() {
+        $('.menu__list').slideToggle();
     });
 
 });
